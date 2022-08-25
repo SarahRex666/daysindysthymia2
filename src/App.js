ada964cad4f8d5a8470fda072f0dee273e983a53
SarahRex <sarahrexmakes@gmail.com>
2022-08-25 13:25:19 -0400

    fixed icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Days In Dysthymia</h1>
+      <h4>
+        "Reduce intellectual and emotional noise until you arrive at the silence
+        of yourself, and listen to it."
+        <br />
+        -Richard Brautigan
+      </h4>
     </div>
   );
 }
